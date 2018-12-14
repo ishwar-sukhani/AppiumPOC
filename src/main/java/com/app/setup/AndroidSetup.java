aba2480d5814f9d327970dfea2807d7c7d5c27f0
Supreet Gandhi <supreetgandhi@instaremlocal.com>
2018-12-14 12:02:12 +0530

    Android Login Script 14-Dec

--- src/main/java/com/app/setup/AndroidSetup.java
+++ src/main/java/com/app/setup/AndroidSetup.java
@@ -3,46 +3,57 @@ package com.app.setup;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
-
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.remote.MobileCapabilityType;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.ios.IOSElement;
+import io.appium.java_client.remote.MobileCapabilityType;
 
 public class AndroidSetup {
 	
-	protected AndroidDriver driver;
+	protected AndroidDriver driver = null;
 
-    protected void prepareAndroidForAppium() throws MalformedURLException,InterruptedException {
+    protected void prepareAndroidForAppium() {
     	
-        File appDir = new File("C:\\Users\\supreetgandhi\\eclipse-workspace\\AndroidTest\\src\\test\\resources\\com\\insta\\apps\\");
-        File app = new File(appDir, "app-testing-v3.apk");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("device","Android");
-
-        //mandatory capabilities
-        capabilities.setCapability("deviceName","Moto G (5s) Plus 3301");
-        capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("appActivity", "com.instarem.mobileapp.MainActivity");
-        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "60");
-        //other caps
-        //capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiAutomator2");
-        capabilities.setCapability("app", app.getAbsolutePath());
-        driver =  new AndroidDriver<AndroidElement>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
-        
-        Thread.sleep(5000);
-        //driver.closeApp();
-        //driver.findElement(By.name("log-in-button")).click();
-        //driver.findElementByAndroidUIAutomator("UiSelector().description(\"log-in-button\")").click();
-       // driver.findElementByLinkText("Log In");
-        //driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc='log-in-button']")).click();
-        //driver.findElement(By.xpath("//android.view.ViewGroup[@text='Log In']")).click();
-        
-        driver.findElementByAccessibilityId("log-in-button").click();
-        driver.			
-        System.out.println("ALL DONE...");
+    	try {
+    		File appDir = new File("C:\\Users\\supreetgandhi\\eclipse-workspace\\AndroidTest\\src\\test\\resources\\com\\insta\\apps\\");
+            File app = new File(appDir, "app-testing-v3.apk");
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            //capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiAutomator2");
+            capabilities.setCapability("device","Android");
+            capabilities.setCapability("deviceName","Moto G (5s) Plus 3301");
+            capabilities.setCapability("platformName","Android");
+            capabilities.setCapability("platformVersion","7.1.1");
+            capabilities.setCapability("appActivity", "com.instarem.mobileapp.MainActivity");
+            capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "60");
+            capabilities.setCapability("app", app.getAbsolutePath());
+            driver =  new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+            Thread.sleep(5000);
+            //AndroidElement loginBtn = driver.findElementByAccessibilityId("log-in-button");
+            //loginBtn.click();
+           // driver.findElement(By.xpath("//android.widget.ViewGroup[@content-desc='log-in-button']")).click();
+            //el.click();
+           driver.findElementByAccessibilityId("log-in-button").click();
+           Thread.sleep(5000);
+           driver.findElementByAccessibilityId("loginnewform_email").sendKeys("test003@test001.com");
+           driver.findElementByAccessibilityId("loginnewform_password").sendKeys("P@ssw0rd1");
+           driver.findElementByAccessibilityId("login-button").click();
+           System.out.println("ALL DONE...");
+    	}
+    	catch(Exception e) {
+    		System.out.println("In CATCH...");
+    		e.printStackTrace();
+    		driver.closeApp();
+    	}
     }
     
     public static void main(String[] args) throws MalformedURLException,InterruptedException {
